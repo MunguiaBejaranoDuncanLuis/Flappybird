7f392eea78d3c9631afe33986f1c26e3282b3a37
CabreraGarciaEduardoEmmanuel <143464606+CabreraGarciaEduardoEmmanuel@users.noreply.github.com>
2024-05-10 18:59:42 +0000

    Version1

---
+++ build/CMakeFiles/dom.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/automerge.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/automerge.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/blink.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/blink.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/bold.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/bold.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/border.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/border.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/box_helper.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/box_helper.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/canvas.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/canvas.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/clear_under.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/clear_under.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/color.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/color.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/composite_decorator.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/composite_decorator.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/dbox.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/dbox.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/dim.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/dim.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/flex.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/flex.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/flexbox.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/flexbox_config.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/flexbox_helper.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_helper.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/focus.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/focus.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/frame.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/frame.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/gauge.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/gauge.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/graph.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/graph.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/gridbox.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/gridbox.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/hbox.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/hbox.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/hyperlink.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/hyperlink.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/inverted.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/inverted.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/linear_gradient.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/linear_gradient.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/node.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/node.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/node_decorator.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/node_decorator.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/paragraph.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/paragraph.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/reflect.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/reflect.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/scroll_indicator.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/scroll_indicator.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/separator.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/separator.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/size.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/size.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/spinner.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/spinner.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/strikethrough.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/strikethrough.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/table.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/table.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/text.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/text.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/underlined.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/underlined.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/underlined_double.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/underlined_double.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/util.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/util.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/dom/vbox.cpp" "/workspaces/Flappybird/build/CMakeFiles/dom.dir/src/ftxui/dom/vbox.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/workspaces/Flappybird/FTXUI/src"
+  "/workspaces/Flappybird/FTXUI/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/workspaces/Flappybird/build/CMakeFiles/screen.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/screen.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/Flappybird/FTXUI/src/ftxui/screen/box.cpp" "/workspaces/Flappybird/build/CMakeFiles/screen.dir/src/ftxui/screen/box.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/screen/color.cpp" "/workspaces/Flappybird/build/CMakeFiles/screen.dir/src/ftxui/screen/color.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/screen/color_info.cpp" "/workspaces/Flappybird/build/CMakeFiles/screen.dir/src/ftxui/screen/color_info.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/screen/image.cpp" "/workspaces/Flappybird/build/CMakeFiles/screen.dir/src/ftxui/screen/image.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/screen/screen.cpp" "/workspaces/Flappybird/build/CMakeFiles/screen.dir/src/ftxui/screen/screen.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/screen/string.cpp" "/workspaces/Flappybird/build/CMakeFiles/screen.dir/src/ftxui/screen/string.cpp.o"
+  "/workspaces/Flappybird/FTXUI/src/ftxui/screen/terminal.cpp" "/workspaces/Flappybird/build/CMakeFiles/screen.dir/src/ftxui/screen/terminal.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/workspaces/Flappybird/FTXUI/src"
+  "/workspaces/Flappybird/FTXUI/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
